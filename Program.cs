9a49f489c02cab2f848031d2f1bfe5aeb580a160
Clifton Neil Diedericks <007Neilo@gmail.com>
2021-08-02 23:56:47 +0200

    multi consumer

--- Program.cs
+++ Program.cs
@@ -14,17 +14,7 @@ namespace RabbitMQ.Producer
             };
             using var connection = factory.CreateConnection();
             using var channel = connection.CreateModel();
-            channel.QueueDeclare("demo-queue",
-                durable: true,
-                exclusive: false,
-                autoDelete: false,
-                arguments: null
-                );
-
-            var message = new { name = "Producer", Message = "Hello!" };
-            var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
-
-            channel.BasicPublish("","demo-queue",null,body);
+           
         }
     }
 }
---
+++ QueueProducer.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMQ.Producer
+{
+    public static class QueueProducer
+    {
+        public static void Publish(IModel channel)
+        {
+            channel.QueueDeclare("demo-queue",
+               durable: true,
+               exclusive: false,
+               autoDelete: false,
+               arguments: null
+               );
+
+            for (int i = 0; i <= 100; i++)
+            {
+                var message = new { name = "Producer", Message = $"Hello! Count:{i}" };
+                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+                QueueProducer.Publish(channel);
+
+                channel.BasicPublish("", "demo-queue", null, body);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
